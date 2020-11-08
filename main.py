3adf28a78c6a54249381aab078f9db53d68ec810
Teodor Anitoaei <anitoaei.teodor@gmail.com>
2020-11-08 21:59:17 +0200

    Added basic documentation

--- main.py
+++ main.py
@@ -20,22 +20,18 @@ class MainWindow(QtWidgets.QDialog, QtWidgets.QPlainTextEdit):
         super().__init__(parent)
 
         logTextBox = QTextEditLogger(self)
-        # You can format what is printed to text box
         logTextBox.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
         logging.getLogger().addHandler(logTextBox)
-        # You can control the logging level
         logging.getLogger().setLevel(logging.DEBUG)
 
         self._button = QtWidgets.QPushButton(self)
         self._button.setText('Test Me')
 
         layout = QtWidgets.QVBoxLayout()
-        # Add the new logging box widget to the layout
         layout.addWidget(logTextBox.widget)
         layout.addWidget(self._button)
         self.setLayout(layout)
 
-        # Connect signal to slot
         self._button.clicked.connect(self.test)
 
     def test(self):
