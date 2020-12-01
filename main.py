eb3d60a68a72d22d48e08a52cd04de5ab60e7261
stanciuioan1 <ioan.stanciu2000@gmail.com>
2020-12-01 17:45:43 +0200

    Update main.py
    swap GUI using Tkinter instead of PyQt

--- main.py
+++ main.py
@@ -1,48 +1,49 @@
-import sys
-import random
-from PySide2 import QtCore, QtWidgets, QtGui
-import logging
+from tkinter import *
+
+def onStart():
+	print("onStart")
+
+def onStop():
+	print("stop")
+
+def onCongestion():
+	print("cong")
+
+def sendData():
+	print("send data")
 
 
-class QTextEditLogger(logging.Handler):
-    def __init__(self, parent):
-        super().__init__()
-        self.widget = QtWidgets.QPlainTextEdit(parent)
-        self.widget.setReadOnly(True)
 
-    def emit(self, record):
-        msg = self.format(record)
-        self.widget.appendPlainText(msg)
+class UIObjects:
 
+	def __init__(self):
+		self.principal = Tk()
 
-class MainWindow(QtWidgets.QDialog, QtWidgets.QPlainTextEdit):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-
-        logTextBox = QTextEditLogger(self)
-        logTextBox.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
-        logging.getLogger().addHandler(logTextBox)
-        logging.getLogger().setLevel(logging.DEBUG)
-
-        self._button = QtWidgets.QPushButton(self)
-        self._button.setText('Test Me')
-
-        layout = QtWidgets.QVBoxLayout()
-        layout.addWidget(logTextBox.widget)
-        layout.addWidget(self._button)
-        self.setLayout(layout)
-
-        self._button.clicked.connect(self.test)
-
-    def test(self):
-        logging.debug('damn, a bug')
-        logging.info('something to remember')
-        logging.warning('that\'s not right')
-        logging.error('foobar')
+		self.startButton = Button(self.principal, text="Start connection", command=onStart, padx=40, pady=20, fg="blue", bg="white")
+		self.stopButton = Button(self.principal, text="Stop connection", command=onStop, padx=40, pady=20, fg="blue", bg="white")
+		self.congestionButton = Button(self.principal, text="Pierde pachete", command=onCongestion, padx=50, pady=10, fg="blue", bg="white")
+		self.sendButton = Button(self.principal, text="Trimite text", command=sendData, padx=50, pady=10, fg="blue", bg="white")
+		self.text1 = Label(self.principal, text="Mesaj trimis")
+		self.text2 = Label(self.principal, text="Mesaj primit")
+		self.messageToSend = Entry(self.principal, width=30, borderwidth=3)
+		self.messageToReceive = Entry(self.principal, width=30, borderwidth=3)
+	
+		self.startButton.grid(row=0, column=0)
+		self.stopButton.grid(row=0, column=1)
+		self.text1.grid(row=1, column=0)
+		self.text2.grid(row=1, column=1)
+		self.messageToSend.grid(row=2, column=0)
+		self.messageToReceive.grid(row=2, column=1)
+		self.sendButton.grid(row=3, column=0)
+		self.congestionButton.grid(row=3, column=1)
 
 
-app = QtWidgets.QApplication(sys.argv)
-dlg = MainWindow()
-dlg.show()
-dlg.raise_()
-sys.exit(app.exec_())
+	def startInterface(self):
+		self.principal.mainloop()
+
+def main():
+	window = UIObjects()
+	window.startInterface()	
+
+
+main()
