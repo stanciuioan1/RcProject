dc4591eb3a6b66600f6ccde6b5cdf2853b0719de
stanciuioan1 <ioan.stanciu2000@gmail.com>
2021-01-21 14:01:15 +0200

    Setare port din interfata

--- src/main.py
+++ src/main.py
@@ -12,9 +12,6 @@ s_port = int(cfgParser.get('com-cfg', 's_port'))
 d_ip = cfgParser.get('com-cfg', 'd_ip')
 d_port = int(cfgParser.get('com-cfg', 'd_port'))
 
-sender = sender.Sender(s_ip, s_port, d_ip, d_port)
-receiver = receiver.Receiver(d_ip, d_port, s_ip, s_port)
-
 
 
 class UIObjects:
@@ -34,6 +31,8 @@ class UIObjects:
 		pass
 
 	def __init__(self):
+
+		
 		self.principal = Tk()
 
 		self.startButton = Button(self.principal, text="Start connection", command=self.onStart, padx=40, pady=20, fg="blue", bg="white")
@@ -42,56 +41,75 @@ class UIObjects:
 		self.sendButton = Button(self.principal, text="Stop pierdere pachete", command=self.stopCongestion, padx=100, pady=10, fg="blue", bg="white")
 		self.server = Label(self.principal, text="Server")
 		self.client = Label(self.principal, text="Client")
-		self.messageToSend = Entry(self.principal, width=30, borderwidth=3)
-		self.messageToReceive = Entry(self.principal, width=30, borderwidth=3)
+		#self.messageToSend = Entry(self.principal, width=30, borderwidth=3)
+		#self.messageToReceive = Entry(self.principal, width=30, borderwidth=3)
 		self.SliderProbability = Scale(self.principal, from_ = 0, to = 100, orient = HORIZONTAL)
-		self.updateButton = Button(self.principal, text="Seteaza parametri", command=self.updateObj, padx=100, pady=10, fg="blue", bg="white")
+		self.SliderPortSource = Scale(self.principal, from_ = 5000, to = 5010, orient = HORIZONTAL)
+		self.SliderPortDest = Scale(self.principal, from_ = 5000, to = 5010, orient = HORIZONTAL)
+		self.updateProbabilityButton = Button(self.principal, text="Seteaza probabilitate", command=self.updateProbabilitate, padx=100, pady=10, fg="blue", bg="white")
+		self.updatePortButton = Button(self.principal, text="Seteaza Port", command=self.setPort, padx=100, pady=10, fg="blue", bg="white")
+		self.PortSource = Label(self.principal, text="Port sursa")
+		self.PortDest = Label(self.principal, text="Port destinatie")
 
-
-		self.startButton.grid(row=0, column=0)
-		self.stopButton.grid(row=0, column=2)
-		self.server.grid(row=1, column=0)
-		self.client.grid(row=1, column=2)
-		self.messageToSend.grid(row=2, column=0)
-		self.messageToReceive.grid(row=2, column=2)
+		self.startButton.grid(row=1, column=0)
+		self.stopButton.grid(row=1, column=2)
+		self.server.grid(row=0, column=0)
+		self.client.grid(row=0, column=2)
+		#self.messageToSend.grid(row=2, column=0)
+		#self.messageToReceive.grid(row=2, column=2)
 		self.sendButton.grid(row=3, column=0)
 		self.SliderProbability.grid(row=3, column=1)
-		self.updateButton.grid(row=2, column=1)
+		self.updateProbabilityButton.grid(row=2, column=1)
 		self.congestionButton.grid(row=3, column=2)
+		self.SliderPortSource.grid(row = 5, column=0)
+		self.SliderPortDest.grid(row = 5, column=2)
+		self.PortSource.grid(row = 4, column=0)
+		self.PortDest.grid(row = 4, column=2)
+		self.updatePortButton.grid(row = 5, column=1)
 
+		self.port_is_set = 0
 
-	def updateObj(self):
-		receiver.update_probability(self.SliderProbability.get())
+	def updateProbabilitate(self):
+		
+		self.receiver.update_probability(self.SliderProbability.get())
 		print(self.SliderProbability.get())
 
+	def setPort(self):
+		if self.port_is_set == 0:
+			port_source = self.SliderPortSource.get()
+			port_dest = self.SliderPortDest.get()
+			self.sender = sender.Sender(s_ip, port_source, d_ip, port_dest)
+			self.receiver = receiver.Receiver(d_ip, port_dest, s_ip, port_source)
+			self.port_is_set = 1
 
 	def startInterface(self):
 		self.principal.mainloop()
-		sender.stop()
-		receiver.stop()
+		self.sender.stop()
+		self.receiver.stop()
 
 	def onStart(self):
 		print("onStart")
-		sender.start()
-		receiver.start()
+		self.sender.start()
+		self.receiver.start()
 
 	def onStop(self):
 		print("stop")
-		receiver.stop()
-		sender.stop()
+		self.receiver.stop()
+		self.sender.stop()
 
 	def startCongestion(self):
-		receiver.is_congested(True)
+		self.receiver.is_congested(True)
 		print("congestion")
 
 	def stopCongestion(self):
-		receiver.is_congested(False)
+		self.receiver.is_congested(False)
 		print("send data")
 
 
 
 
 def main():
+	
 	window = UIObjects()
 	window.startInterface()	
 
